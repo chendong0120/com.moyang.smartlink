17c3d1a2e39f7ad17049fdce7c01d262d677ab74
陈东东 <kevinchen@infapp.com>
2015-09-08 02:24:32 +0800

    添加了timer

--- src/ios/smartlink.m
+++ src/ios/smartlink.m
@@ -7,23 +7,26 @@
     // Member variables go here.
  
 }
+@property (nonatomic, strong) NSTimer *paintingTimer;
 
 - (void)startSmartLink:(CDVInvokedUrlCommand*)command;
-- (void)getSSid:(CDVInvokedUrlCommand*)command;
-//- (void)getCurrentSSID:(CDVInvokedUrlCommand*)command;
-@property (strong,nonatomic) CDVInvokedUrlCommand * commandHolder;
-
+//-(void)getSSid;
+- (void)getCurrentSSID:(CDVInvokedUrlCommand*)command;
 @end
 
-
 @implementation smartlink{
-    HFSmartLink * smtlk;
-    BOOL isconnecting;
+HFSmartLink * smtlk;
+BOOL isconnecting;
       CDVPluginResult *pluginResult;
+    CDVInvokedUrlCommand *publicCommand;
+    NSDictionary  *ret;
+   
+    NSString *errorMessage;
 }
-@synthesize commandHolder;
+
 - (void)startSmartLink:(CDVInvokedUrlCommand*)command
 {
+    
     // Do any additional setup after loading the view, typically from a nib.
     smtlk = [HFSmartLink shareInstence];
     smtlk.isConfigOneDevice = true;
@@ -32,73 +35,33 @@
     NSString *ssid=command.arguments[0];
     NSString *pwd=command.arguments[1];
  
-    self.commandHolder=command;
+    publicCommand=command;
     
     [smtlk startWithKey:pwd processblock:^(NSInteger process) {
        // self.progress.progress = process/18.0;
     } successBlock:^(HFSmartLinkDeviceInfo *dev) {
-        //NSLog(dev.mac);
-        NSDictionary  *ret =nil;
         
-       ret= [NSDictionary dictionaryWithObjectsAndKeys:
+        ret =
+        [NSDictionary dictionaryWithObjectsAndKeys:
          dev.mac, @"Mac",
          dev.ip, @"ModuleIPc",
          @"",@"Mid",
          @"",@"Info",
+         @"",@"error",
          nil];
        
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:ret];
-        
-        [self.commandDelegate sendPluginResult:pluginResult callbackId:self.commandHolder.callbackId];
     } failBlock:^(NSString *failmsg) {
-        
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:failmsg];
-        
-        [self.commandDelegate sendPluginResult:pluginResult callbackId:commandHolder.callbackId];
-        
+                 errorMessage=failmsg;
+
         } endBlock:^(NSDictionary *deviceDic) {
-            
-            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsDictionary:deviceDic];
-            
-            [self.commandDelegate sendPluginResult:pluginResult callbackId:commandHolder.callbackId];
-            isconnecting  = false;
-            
+//            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsDictionary:deviceDic];
+//            [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+//            isconnecting  = false;
     }];
-   
+    [self startPainting ];
+
 }
-//- (void)getSSid
-//{
-//    BOOL wifiOK= FALSE;
-//    NSDictionary *ifs;
-//    NSString *ssid;
-//      if (!wifiOK)
-//    {
-//        ifs = [self fetchSSIDInfo];
-//        ssid = [ifs objectForKey:@"SSID"];
-//        if (ssid!= nil)
-//        {
-//            wifiOK= TRUE;
-//             pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:ssid];
-//            
-//        }
-//        else
-//        {
-//             pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"请链接网络"];
-//        }
-//    }
-//}
-//
-//- (id)fetchSSIDInfo {
-//    NSArray *ifs = (__bridge_transfer id)CNCopySupportedInterfaces();
-//    NSLog(@"Supported interfaces: %@", ifs);
-//    id info = nil;
-//    for (NSString *ifnam in ifs) {
-//        info = (__bridge_transfer id)CNCopyCurrentNetworkInfo((__bridge CFStringRef)ifnam);
-//        NSLog(@"%@ => %@", ifnam, info);
-//        if (info && [info count]) { break; }
-//    }
-//    return info;
-//}
+
 
 - (void)getSSid:(CDVInvokedUrlCommand*)command
 {
@@ -121,5 +84,57 @@
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
     }
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+
+
+
+
+// 定时器执行的方法
+- (void)paint:(NSTimer *)paramTimer{
+    
+    NSLog(@"定时器执行的方法");
+    
+    
+    if(errorMessage!=nil){
+        [self stopPainting];
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:errorMessage];
+            [self.commandDelegate sendPluginResult:pluginResult callbackId:publicCommand.callbackId];
+    }
+    if(ret!=nil){
+        
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:ret];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:publicCommand.callbackId];
+
+        }
+}
+
+- (void) startPainting{
+    //    seconds：需要调用的毫秒数
+    //    target：调用方法需要发送的对象。即：发给谁
+    //    userInfo：发送的参数
+    //    repeats：指定定时器是否重复调用目标方
+    // 定义一个NSTimer
+    // 定义将调用的方法
+    SEL selectorToCall = @selector(paint:);
+    // 为SEL进行 方法签名
+    NSMethodSignature *methodSignature =[[self class] instanceMethodSignatureForSelector:selectorToCall];
+    // 初始化NSInvocation
+    NSInvocation *invocation =[NSInvocation invocationWithMethodSignature:methodSignature];
+    [invocation setTarget:self];
+    [invocation setSelector:selectorToCall];
+    self.paintingTimer = [NSTimer timerWithTimeInterval:1.0
+                                             invocation:invocation
+                                                repeats:YES];
+    
+    // 当需要调用时,可以把计时器添加到事件处理循环中
+    [[NSRunLoop currentRunLoop] addTimer:self.paintingTimer forMode:NSDefaultRunLoopMode];
+}
+// 停止定时器
+- (void) stopPainting{
+    if (self.paintingTimer != nil){
+        // 定时器调用invalidate后，就会自动执行release方法。不需要在显示的调用release方法
+        [self.paintingTimer invalidate];
+    }
 }
 @end
